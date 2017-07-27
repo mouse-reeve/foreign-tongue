6474adb79ac9c9f6c6cefbc81133a56d5d6d58f0
Mouse Reeve <mousereeve@riseup.net>
2017-07-27 15:40:33 -0700

    Version numbers whatever

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='ForeignTongue',
-    version='0.0.1',
+    version='0.1.0',
 
     description='Generates new languages',
     long_description=long_description,
